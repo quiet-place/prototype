b2991c3a547a7b274e5bead4c0342826586aa384
rbwo <rbwo552@naver.com>
2022-04-06 14:58:06 +0900

    niber_hongki

---
+++ niber_hongki/team3_prototype2/team3_prototype2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  team3_prototype2
+//
+//  Created by rbwo on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ niber_hongki/team3_prototype2/team3_prototype2/team3_prototype2App.swift
@@ -0,0 +1,17 @@
+//
+//  team3_prototype2App.swift
+//  team3_prototype2
+//
+//  Created by rbwo on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct team3_prototype2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
