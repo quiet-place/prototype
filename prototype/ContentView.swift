a5e1c64035626ad54e515fc17ea00688bc001d92
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-04-06 14:27:27 +0900

    Initial Commit

---
+++ prototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prototype
+//
+//  Created by apple developer academy on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ prototype/prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototypeApp.swift
+//  prototype
+//
+//  Created by apple developer academy on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
