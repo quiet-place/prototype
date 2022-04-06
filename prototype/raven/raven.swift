cb412ffb37bcbedf88f709e1b5652f903c3f872f
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-04-06 14:35:36 +0900

    Chore: create raven swiftUI file

---
+++ prototype/raven/raven.swift
@@ -0,0 +1,20 @@
+//
+//  raven.swift
+//  prototype
+//
+//  Created by apple developer academy on 2022/04/06.
+//
+
+import SwiftUI
+
+struct raven: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct raven_Previews: PreviewProvider {
+    static var previews: some View {
+        raven()
+    }
+}
